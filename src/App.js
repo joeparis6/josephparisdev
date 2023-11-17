9734bcdfbd59953448574e883701cd08b570198e
joeparis6 <josephparis6@gmail.com>
2023-11-16 21:42:28 -0700

    added home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="Navbar">
+        <nav>
+          <button>item 1</button>
+          <button>item 2</button>
+          <button>item 3</button>
+          <button>item 4</button>
+        </nav>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Joseph Paris
+        </h2>
+        <h5>
+          Serverless Web Developer
+        </h5>
+        <div>
+          <h5>
+            Education
+          </h5>
+          <ul>
+          <li>BS Computer Science - Brigham Young University 2022</li>
+          </ul>
+        </div>
+        <div>
+          <h5>
+            Experience
+          </h5>
+          <ul>
+          <li>Full Stack React Developer @ Lion Energy Jan 2023 - Present</li>
+          </ul>
+        </div>
       </header>
+      
     </div>
   );
 }
