ea23c3874309e5f2a497d8a0282dba6a4c209b4e
joeparis6 <josephparis6@gmail.com>
2024-01-06 16:26:11 -0700

    added button labels

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="Navbar">
+      
         <nav>
-          <div style={{ display:'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-            <div>
-              <button>item 1</button>
-              <button>item 2</button>
-              <button>item 3</button>
-              <button>item 4</button>
+          <div className="Navbar">
+            <div className='Button-group'>
+              <button>Experience</button>
+              <button>Education</button>
+              <button>Skills</button>
+              <button>Goals</button>
             </div>
             <button>
-              login
+                login
             </button>
           </div>
-          
         </nav>
         
-      </div>
       
-      <header className="App-header">
+      <div className='App-content'>
+        <header className="App-header">
         <h2>
           Joseph Paris
         </h2>
         <h5>
           Serverless Web Developer
         </h5>
-        <div>
+        <div className='Resume'>
+          <div className='Resume-item'>
           <h5>
             Education
           </h5>
@@ -36,7 +36,7 @@ function App() {
           <li>BS Computer Science - Brigham Young University 2022</li>
           </ul>
         </div>
-        <div>
+        <div className='Resume-item'>
           <h5>
             Experience
           </h5>
@@ -44,7 +44,12 @@ function App() {
           <li>Full Stack React Developer @ Lion Energy Jan 2023 - Present</li>
           </ul>
         </div>
+        </div>
+        
       </header>
+      </div>
+      
+      
       
     </div>
   );
